fa6722ef3b1e29fed97e74d01fb94e13131064ff
Roshanchenna <roshanchenna@gmail.com>
2025-01-09 13:29:04 +0530

    modified

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get("/FI", (req, res) => {
   res.send("FI is working");
 });
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
 });
+
