36542579613196f6f201893dbe49768b25c9d440
Roshanchenna <roshanchenna@gmail.com>
2025-01-30 14:51:44 +0530

    redis added

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const redis = require("redis");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
+
+
+// connection for redis
+const redisClient = redis.createClient({
+  socket: {
+    host: 'master.redis-cache-1a-test.sudini.aps1.cache.amazonaws.com',
+    port: 6379,
+    connectTimeout: 10000
+  },
+});
+
+console.log('Attempting to connect to Redis...');
+
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (error) {
+    console.error('Redis connection failed:', error);
+  }
+})();
+
 
 // Webhook endpoint
 app.post("/FI/Notification", (req, res) => {
